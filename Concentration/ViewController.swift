4badf87275677bd55f3501a4bd02e05cc3847848
Yoonie <kdy0096@gmail.com>
2023-08-16 00:55:26 +0900

    initialize

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cardButton: UIButton!
+    var ghostIsOn: Bool = false
+    let objects: [String] = ["👻"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func cardPressed(_ sender: UIButton) {
+        cardFlipped(card: sender)
+        ghostIsOn = !ghostIsOn
+        
+    }
+    
+    func cardFlipped(card: UIButton) {
+        if ghostIsOn {
+            print(ghostIsOn)
+            card.setTitle("", for: .normal)
+            card.backgroundColor = .systemOrange
+        } else{
+            card.setTitle(objects[0], for: .normal)
+            card.size
+            card.backgroundColor = .white
+        }
+    }
 }
 
