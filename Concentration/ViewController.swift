7ea5f8e6622bfa0f0bdf030ea57d0d6ca8eac379
Yoonie <kdy0096@gmail.com>
2023-08-15 23:01:40 +0900

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by yoonie on 2023/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
