646beca47795b4dd1cfa470eacb191711c32223c
Doyoon Kim <kdy0096@gmail.com>
2023-08-21 09:58:24 +0900

    "duplicate ghost button / func touchCard/flipCard"

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,29 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var flipCount = 0
+    
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
         flipCard(withEmoji: "👻", on: sender)
     }
     
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
     func flipCard(withEmoji emoji: String, on button: UIButton) {
-        print("clicked")
-        if let buttonText = button.titleLabel?.text{
-            print(buttonText)
-            if let buttonText = button.currentTitle {
-                if buttonText == "" {
-                    button.setTitle(emoji, for: UIControl.State.normal)
-                    print("2")
-                    print(button.titleLabel!.text!)
-                    print(button.currentTitle!)
-                }
-            }
-            if buttonText == emoji {
-                button.setTitle("", for: UIControl.State.normal)
-                print("1")
-                print(button.titleLabel!.text!)
-                print(button.currentTitle!)
-            }
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         }
     }
+    
+    
+
 }
 
